6221fd37b06fe15ca2d1cfbcdeb4a82ba023b088
Daniela A <daniela_3102@outlook.com>
2022-10-16 20:00:32 -0500

    Se mofinalizo el desarrollo de la funcionalidad Crear, se validaron las excepciones

--- Crear.php
+++ Crear.php
@@ -12,6 +12,7 @@
 <?php
     require_once('class/Actividad.php');
     require_once('class/TipoActividad.php');
+    $valorRes = 0;
 ?>
     <!-- Menu de la aplicacion -->
 <ul>
@@ -41,10 +42,12 @@
                     <!-- fecha 24 horas -->     
                     <input id='h' type='time' name='horaR' value="23:53:00"> <br> <br>
                     <?php
-                        if($_REQUEST['rs'] == "si"){
-                            $valorRes = "23:53";
-                        }else{
-                            $valorRes = $_REQUEST['horaR'];
+                        if(array_key_exists('rs', $_POST)){
+                            if($_REQUEST['rs'] == "si"){
+                                $valorRes = "23:53";
+                            }else{
+                                $valorRes = $_REQUEST['horaR'];
+                            }   
                         }
                     ?>
                 <!-- Select para elegir el tipo de actividad -->
@@ -75,16 +78,22 @@
     </div>
 </div>
 <?php
-    $obj_act = new Actividad();
-    $registrar = $obj_act->registrarAct($_REQUEST['titulo'], $_REQUEST['fecha'], $_REQUEST['hora'], $_REQUEST['ubicacion'], 
-                                        $_REQUEST['email'], $valorRes, $_REQUEST['tiposA']);
-    if($registrar > 0){
-       // echo "Se ingresó correctamente";
-        print("<script> alert('Se ingresó correctamente'); </script>");
-        print("<script type='text/javascript'> window.location.href = 'ProyectPrincipal.php'; </script>");
-    }else{
-        //echo "No se ingresó correctamente";
-        print("<script> alert('Revisar Campos'); </script>");
+    if(array_key_exists('titulo', $_POST)  && array_key_exists('fecha', $_POST) &&
+       array_key_exists('hora', $_POST)  && array_key_exists('ubicacion', $_POST) &&
+       array_key_exists('email', $_POST) && array_key_exists('tiposA', $_POST)){
+
+        $obj_act = new Actividad();
+        $registrar = $obj_act->registrarAct($_REQUEST['titulo'], $_REQUEST['fecha'], $_REQUEST['hora'], $_REQUEST['ubicacion'], 
+                                            $_REQUEST['email'], $valorRes, $_REQUEST['tiposA']);
+        if($registrar > 0){
+        //Se ingresó correctamente
+            print("<script> alert('Se ingresó correctamente'); </script>");
+            //Reedirecciona hacia la página principal de gestión de actividades
+            print("<script type='text/javascript'> window.location.href = 'ProyectPrincipal.php'; </script>");
+        }else{
+            //No se ingresó correctamente
+            print("<script> alert('Revisar Campos'); </script>");
+        }
     }
 ?>
 
