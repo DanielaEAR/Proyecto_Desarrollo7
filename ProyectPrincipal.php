ed2192792a9b3a0107d7cb15175e279b841b3765
Hguerrero0821 <hectorc0822@gmail.com>
2022-10-16 20:52:00 -0500

    modificacion del archivo proyectprincipal

--- ProyectPrincipal.php
+++ ProyectPrincipal.php
@@ -9,5 +9,41 @@
 </head>
 <body>
     
+    <!-- Menu de la aplicacion -->
+<ul>
+  <li><a href="ResumenActividad.php">Inicio</a></li>
+  <li><a href="#">Mis actividades</a></li>
+  <li><a href="#">Reportses</a></li>
+</ul>
+<div class="col-md-8">
+
+<h1>Tareas pendientes</h1> <br> <br>
+
+<table class="table">
+
+    <thead>
+
+        <tr>
+            <th>Titulo</th>
+        </tr>
+
+    </thead>
+
+    <tbody>
+       
+        <tr>
+            <th>hector</th>
+            <th><a href="editar.php">Editar</a></th>
+            <th><a href="delete.php">Eliminar</a></th>   
+        </tr>
+    </tbody>
+
+</table>
+
+</div>
+
+</div>
+<br><br>
+<button><a href="Crear.php">CREAR ACTIVIDAD</a></button>
 </body>
 </html>
